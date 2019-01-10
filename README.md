cb16d5226e0c0a13a734c28ba457c2a51fae90f1
Victor Lauria <vlauria3778@conestogac.on.ca>
2019-01-09 19:27:18 -0500

    Generated custom components

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Angular101
+A simple Angular project from a crash course for beginners
+
+Reference: https://www.youtube.com/watch?v=z4JUm0Bq9AM
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+// Decorator of the component
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+// Logic of the component
 export class AppComponent {
   title = 'angular101';
 }
