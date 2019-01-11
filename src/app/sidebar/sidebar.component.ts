144493977065ec530ec71343fa17e4cb5a8ede22
Victor Lauria <vlauria3778@conestogac.on.ca>
2019-01-10 21:08:04 -0500

    Fixed active background color for sidebar icons

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -11,7 +11,11 @@ export class SidebarComponent implements OnInit {
   currentUrl: string;
 
   constructor(private router: Router) {
-    router.events.subscribe((_: NavigationEnd) => this.currentUrl = _.url);
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentUrl = event.url;
+      }
+    });
   }
 
   ngOnInit() {}
